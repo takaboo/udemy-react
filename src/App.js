58e76987133f7ad327005ef3261a65db87d0eccf
小林貴光 <takaryon@kobayashitakamitsunoMacBook-Pro.local>
2020-03-03 00:39:07 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <h1>hello , world</h1>
-  );
+// function App() {
+//   return (
+//     <div><h1>hi!</h1></div>
+//   );
+// }
+
+// export default App;
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input id="bar" type="text" onChange={()=>{console.log("iam change");}} />
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
