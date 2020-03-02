9afaaf58dc792e2b80e168b049c9eba0b479d3b0
小林貴光 <takaryon@kobayashitakamitsunoMacBook-Pro.local>
2020-03-03 00:52:45 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -8,15 +8,29 @@ import React, {Component} from 'react';
 
 // export default App;
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input id="bar" type="text" onChange={()=>{console.log("iam change");}} />
-      </React.Fragment>
-    )
-  }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input id="bar" type="text" onChange={()=>{console.log("iam change");}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Moew!</div>
 }
 
 export default App;
