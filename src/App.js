23f8ed52cc475b7b4785b83b10126f5bf288b82d
小林貴光 <takaryon@kobayashitakamitsunoMacBook-Pro.local>
2020-03-03 23:40:55 +0900

    implement action creator

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
 
-const App = () => {
-  const profiles = [
-    {name: "taro",age: 10},
-    {name: "Hanako", age: 5},
-    {name: "yamada", age: 19}
-  ]
+const App = () => (<Counter></Counter>)
+
+
+class Counter extends Component {
+  constructor(props){
+    super(props);
+    this.state = {count: 0}
+  }
+  handlePlusButton = () => {
+    this.setState({count: this.state.count + 1});
+  }
+  handleMainasuButton = () => {
+    this.setState({count: this.state.count - 1});
+  }
+  render() {
+    console.log("render");
   return (
-    <div>
-      {
-        profiles.map((profile, index)=>{
-          return <User name={profile.name} age={profile.age} key={index}/>
-        })
-      }
-    </div>
-  )
-}
-
-const User = (props) => {
-return <div>Hi, I am {props.name}, and {props.age} years old</div>
-}
-
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
+    <React.Fragment>
+      <div>counter: {this.state.count}</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMainasuButton}>-1</button>
+    </React.Fragment>
+    )
+  }
 }
 
 export default App;
---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT';
+export const DECRIMENT = 'DECREMENT';
+
+export const increment = () => ({
+    type: INCREMENT
+})
+
+export const decrement = () => ({
+    type: DECRIMENT
+})
