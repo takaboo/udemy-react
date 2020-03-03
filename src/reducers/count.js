8ef933744fc93da0c14af24c15a88ede8c2da55f
小林貴光 <takaryon@kobayashitakamitsunoMacBook-Pro.local>
2020-03-04 00:39:11 +0900

    修正

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,10 +1,10 @@
-import {INCREMNT, DECRIMENT} from '../actions'
+import {INCREMENT, DECRIMENT} from '../actions'
 
 const initialState = {value: 0}
 
 export default (state = initialState, action) => {
   switch(action.type){
-    case INCREMNT:
+    case INCREMENT:
       return {value: state.value+1}
     case DECRIMENT:
       return {value: state.value -1}
