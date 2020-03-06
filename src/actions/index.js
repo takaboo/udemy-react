964320abaf0b9993dd231107ea6ffe9cb90e00cc
小林貴光 <takaryon@kobayashitakamitsunoMacBook-Pro.local>
2020-03-07 00:52:59 +0900

    action index

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
-export const INCREMENT = 'INCREMENT';
-export const DECRIMENT = 'DECREMENT';
+import axios from 'axios'
 
-export const increment = () => ({
-    type: INCREMENT
-})
+export const READ_EVENTS = 'READ_EVENTS';
 
-export const decrement = () => ({
-    type: DECRIMENT
-})
+const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1';
+const QUERYSTRING = '?token=token123';
+
+export const readEvents= () => async dispatch=> {
+    const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+    dispatch({type: READ_EVENTS, response})
+}
